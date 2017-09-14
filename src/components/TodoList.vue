65ef1dc86bb149489d2816eefb3da43c8876b54e
John Blossom <jblossom.web@gmail.com>
2017-09-14 16:05:20 -0600

    create Todo component, loop

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <ul>
-        <li> Todo A </li> 
-        <li> Todo B </li> 
-        <li> Todo C </li> 
-    </ul> 
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <hr />
+    <todo v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:key="index"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
 
+import Todo from './Todo';
+
 export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
 };
 </script>
 <style>
