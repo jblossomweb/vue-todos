0f1ff704f17cafa6c261679d998e2d9093f1fd09
John Blossom <jblossom.web@gmail.com>
2017-09-20 14:17:21 -0600

    relocate css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import 'semantic-ui-css/semantic.min.css';
+import './style.css';
 import App from './App';
 import TodoService from './services/Todo.service';
 
